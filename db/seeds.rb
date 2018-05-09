38726a8765e4ea7d06350f326effff193cd0f4dd
Aaron Christy <aaron@phad.com>
2018-05-08 22:26:20 -0400

    Add seeds to create default survey with questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Survey.where(name: "Spiritual Gifts").first_or_initialize do |survey|
+  puts "Creating survey '#{survey.name}' with default questions."
+
+  CSV.foreach(Rails.root.join("docs/data/questions.csv"), headers: true) do |row|
+    survey.questions.build(text: row[0].chomp)
+  end
+  survey.save!
+end
