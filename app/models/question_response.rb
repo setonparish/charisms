8c7b3e5b0289377e417cc6977e25a5659d3924ed
Aaron Christy <aaron@phad.com>
2018-05-29 23:25:36 -0400

    Add scopes / interrogation methods for answers

--- app/models/question_response.rb
+++ app/models/question_response.rb
@@ -3,4 +3,16 @@ class QuestionResponse < ApplicationRecord
   belongs_to :question
 
   validates :question, uniqueness: { scope: [:survey_response] }
+
+  def self.unanswered
+    where(answer: nil)
+  end
+
+  def answered?
+    !unanswered?
+  end
+
+  def unanswered?
+    answer.blank?
+  end
 end
--- spec/models/question_response_spec.rb
+++ spec/models/question_response_spec.rb
@@ -8,4 +8,29 @@ describe QuestionResponse do
       expect(question_response).to be_valid
     end
   end
+
+  describe ".answered" do
+    let!(:answered_question_response) { FactoryBot.create(:question_response, answer: "so much to be consoled") }
+    let!(:unanswered_question_response) { FactoryBot.create(:question_response, answer: nil) }
+
+    it "returns answered question_responses" do
+      expect(QuestionResponse.unanswered).to match_array(unanswered_question_response)
+    end
+  end
+
+  describe "#answered?" do
+    let(:question_response) { FactoryBot.build(:question_response, answer: "so much to be consoled") }
+
+    it "is a valid factory" do
+      expect(question_response).to be_answered
+    end
+  end
+
+  describe "#unanswered?" do
+    let(:question_response) { FactoryBot.build(:question_response, answer: nil) }
+
+    it "is a valid factory" do
+      expect(question_response).to be_unanswered
+    end
+  end
 end
