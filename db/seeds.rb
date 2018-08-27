39edbc753df887d9a59e93756b5d247e9619e333
Aaron Christy <aaron@phad.com>
2018-08-27 13:03:15 -0400

    Ensure that "all" seeds are run prior to environment specific seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-[Rails.env, "all"].each do |seed|
+["all", Rails.env].each do |seed|
   seed_file = "#{Rails.root}/db/seeds/#{seed}.rb"
   if File.exists?(seed_file)
     puts "*** Loading #{seed} seed data"
