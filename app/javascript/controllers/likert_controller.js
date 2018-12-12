b55d7c7599004fea467a39454502b18b790b043e
Aaron Christy <aaron@phad.com>
2018-12-12 15:51:58 -0500

    Improve question collection UI
    * form will auto submit to next question
    * selecting a radio button will keep background color

---
+++ app/javascript/controllers/likert_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["form", "label", "radio"];
+
+  initialize() {
+    this.highlight();
+  }
+
+  select() {
+    this.highlight();
+    this.submit();
+  }
+
+  submit() {
+    this.formTarget.submit();
+  }
+
+  highlight() {
+    this.clearHighlights();
+    this.addHighlightToSelectedRadio();
+  }
+
+  clearHighlights() {
+    this.labelTargets.forEach(function (el) {
+      el.classList.remove("selected");
+    });
+  }
+
+  addHighlightToSelectedRadio() {
+    this.radioTargets.forEach(function (el) {
+      if (el.checked) {
+        el.closest('label').classList.add("selected");
+      }
+    });
+  }
+}
